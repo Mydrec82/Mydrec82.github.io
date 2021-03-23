0c998a1e82d04316e1e509415c78e420aa50fc25
YSergey <mudrec82@gmail.com>
2021-03-23 21:30:55 +0300

    some test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+
+const triangles = document.querySelectorAll('.first__triangle');
+const circles = document.querySelectorAll('.first__circle');
+
+circles[1].after(triangles[2]);
+
+circles[0].textContent = '56';
